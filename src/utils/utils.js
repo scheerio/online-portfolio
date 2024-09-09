8cb6970fb94c4b05d3643435982ef794520c6995
Ben Scheer <benjack321@gmail.com>
2024-09-09 00:03:41 -0400

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -273,7 +273,7 @@ const personalLinks = [
   {
     id: 2,
     title: "Resume 📄",
-    link: "https://drive.google.com/file/d/1yJLv98KVEqwfaIx2YWJHjbsIIc1Xkilx/view?usp=sharing",
+    link: "https://drive.google.com/file/d/123ALdDVliME5upgKgEN0blgPK_s8imd_/view?usp=sharing",
   },
 ];
 
